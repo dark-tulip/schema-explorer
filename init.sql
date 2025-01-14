53cf6dac91b64aa7b796b1dcf99a36e774642d1a
ttansh <ttelbayeva@innoforce.kz>
2025-01-14 16:20:24 +0500

    Добавление тестовой таблицы books в базе данных источнике

--- init.sql
+++ init.sql
@@ -9,3 +9,24 @@ ALTER DATABASE source_db OWNER TO user1;
 CREATE DATABASE sink_db;
 CREATE USER user2 WITH PASSWORD 'user2pwd';
 ALTER DATABASE sink_db OWNER TO user2;
+
+-- =========================== 1.1 Подключение к source_db ===============================
+\connect source_db;
+
+-- Создание таблицы и вставка данных
+CREATE TABLE books
+(
+    id             BIGSERIAL PRIMARY KEY,
+    title          text NOT NULL,
+    author         text NOT NULL,
+    published_year INTEGER
+);
+
+INSERT INTO books (title, author, published_year)
+VALUES ('1984', 'George Orwell', 1949),
+       ('To Kill a Mockingbird', 'Harper Lee', 1960),
+       ('The Great Gatsby', 'F. Scott Fitzgerald', 1925),
+       ('Pride and Prejudice', 'Jane Austen', 1813),
+       ('The Catcher in the Rye', 'J.D. Salinger', 1951);
+
+-- ======================================================================================
